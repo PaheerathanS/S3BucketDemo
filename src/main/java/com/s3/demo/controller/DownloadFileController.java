4fd836811ce2ca84482ec895da30aea000245c2a
Paheerathan <paheerathan03@gmail.com>
2019-07-31 17:05:25 +0530

    Initial commit

---
+++ src/main/java/com/s3/demo/controller/DownloadFileController.java
@@ -0,0 +1,43 @@
+package com.s3.demo.controller;
+
+import java.io.ByteArrayOutputStream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.s3.demo.services.S3Services;
+
+@RestController
+public class DownloadFileController {
+	@Autowired
+	S3Services s3Services;
+
+	@GetMapping("/api/file/{keyname}")
+	public ResponseEntity<byte[]> downloadFile(@PathVariable String keyname) {
+		ByteArrayOutputStream downloadInputStream = s3Services.downloadFile(keyname);
+
+		return ResponseEntity.ok().contentType(contentType(keyname))
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + keyname + "\"")
+				.body(downloadInputStream.toByteArray());
+	}
+
+	private MediaType contentType(String keyname) {
+		String[] arr = keyname.split("\\.");
+		String type = arr[arr.length - 1];
+		switch (type) {
+		case "txt":
+			return MediaType.TEXT_PLAIN;
+		case "png":
+			return MediaType.IMAGE_PNG;
+		case "jpg":
+			return MediaType.IMAGE_JPEG;
+		default:
+			return MediaType.APPLICATION_OCTET_STREAM;
+		}
+	}
+}
---
+++ src/test/java/com/s3/demo/S3DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.s3.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class S3DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
